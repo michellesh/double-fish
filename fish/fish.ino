685d88dd4eac45706e222f67f9bf15a5fea202aa
Michelle Sharer <michellesharer@gmail.com>
2023-06-03 11:48:54 -0600

    Hardcoded x-axis values for all LEDs

--- fish/fish.ino
+++ fish/fish.ino
@@ -26,6 +26,19 @@ int rightY[] = {171, 171, 173, 176, 181, 188, 199, 210, 223, 236, 251,
 int Y_MIN = 50;
 int Y_MAX = 434;
 
+int leftX[] = {265, 282, 298, 313, 327, 339, 350, 360, 369, 376, 380,
+               383, 384, 382, 391, 402, 403, 394, 383, 371, 356, 341,
+               328, 314, 298, 282, 266, 250, 238, 231, 232, 237, 242,
+               249, 261, 270, 273, 274, 279, 291, 306, 304, 291, 275,
+               263, 249, 235, 231, 238, 245, 235, 233, 244, 244};
+int rightX[] = {170, 154, 137, 122, 106, 92,  78,  67,  57,  48,  41,
+                36,  33,  31,  32,  31,  17,  11,  16,  27,  40,  53,
+                68,  82,  95,  111, 127, 143, 159, 173, 182, 183, 181,
+                175, 168, 158, 147, 142, 141, 138, 128, 113, 106, 118,
+                133, 146, 158, 173, 184, 184, 176, 171, 182, 180};
+int X_MIN = 11;
+int X_MAX = 403;
+
 CRGB leftLEDs[NUM_LEDS];
 CRGB rightLEDs[NUM_LEDS];
 
@@ -42,22 +55,7 @@ void loop() {
   FastLED.clear();
   palette.cycle();
 
-  static int y = Y_MIN;
-  static int yInc = 1;
-  int zThreshold = 5;
-  for (int i = 0; i < NUM_LEDS; i++) {
-    if (isBetween(leftY[i], y - zThreshold, y + zThreshold)) {
-      leftLEDs[i] = CRGB::White;
-    }
-    if (isBetween(rightY[i], y - zThreshold, y + zThreshold)) {
-      rightLEDs[i] = CRGB::White;
-    }
-  }
-
-  y += yInc;
-  if (y > Y_MAX || y < Y_MIN) {
-    yInc *= -1;
-  }
+  testAxisX();
 
   // twinkle();
 
@@ -67,6 +65,44 @@ void loop() {
   // }
 
   FastLED.show();
+}
+
+void testAxisX() {
+  static int x = X_MIN;
+  static int inc = 1;
+  int threshold = 5;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (isBetween(leftX[i], x - threshold, x + threshold)) {
+      leftLEDs[i] = CRGB::White;
+    }
+    if (isBetween(rightX[i], x - threshold, x + threshold)) {
+      rightLEDs[i] = CRGB::White;
+    }
+  }
+
+  x += inc;
+  if (x > X_MAX || x < X_MIN) {
+    inc *= -1;
+  }
+}
+
+void testAxisY() {
+  static int y = Y_MIN;
+  static int inc = 1;
+  int threshold = 5;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (isBetween(leftY[i], y - threshold, y + threshold)) {
+      leftLEDs[i] = CRGB::White;
+    }
+    if (isBetween(rightY[i], y - threshold, y + threshold)) {
+      rightLEDs[i] = CRGB::White;
+    }
+  }
+
+  y += inc;
+  if (y > Y_MAX || y < Y_MIN) {
+    inc *= -1;
+  }
 }
 
 bool isBetween(int value, int range1, int range2) {
